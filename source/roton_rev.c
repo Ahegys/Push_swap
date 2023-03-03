7576a9c31bb9fb6e911258476e53109dc0c75c0c
Akhashi <andersonfelpe123@gmail.com>
2023-03-03 13:33:08 -0300

    add rrb

--- source/roton_rev.c
+++ source/roton_rev.c
@@ -20,3 +20,24 @@ int * swap_rra(t_container * stack)
 	get_stack[0] = stack->stack_a[size];
 	return (get_stack);
 }
+
+int * swap_rrb(t_container * stack)
+{
+	int index;
+	int size;
+	int * get_stack;
+
+	size = 0;
+	while(stack->stack_b[size])
+		size++;
+	get_stack = ft_calloc(size, sizeof(int *));
+	size = size - 1;
+	index = 1;
+	while (index <= size)
+	{
+		get_stack[index] = stack->stack_b[index - 1];
+		index++;
+	}
+	get_stack[0] = stack->stack_b[size];
+	return (get_stack);
+}
