87c07de02e369610d45f690163f9018384afaaf8
Akhashi <andersonfelpe123@gmail.com>
2023-03-06 19:44:42 -0300

    fix error print erro in stack

--- source/short_sort.c
+++ source/short_sort.c
@@ -14,7 +14,7 @@ void	short_sort(t_container * stack)
 {
 	if (validate_stack(stack->stack_a) == 2)
 		sort_two(stack);
-	if (validate_stack(stack->stack_a) == 3)
+	else if (validate_stack(stack->stack_a) == 3)
 		sort_three(stack);
 	else
 		ft_printf("\n stack not valid!\n");
