1cd72602e3160f6bfb4e92843c90c55d0e438bdb
Akhashi <andersonfelpe123@gmail.com>
2023-03-06 19:27:07 -0300

    sort_three completed

--- source/short_sort.c
+++ source/short_sort.c
@@ -14,6 +14,8 @@ void	short_sort(t_container * stack)
 {
 	if (validate_stack(stack->stack_a) == 2)
 		sort_two(stack);
+	if (validate_stack(stack->stack_a) == 3)
+		sort_three(stack);
 	else
 		ft_printf("\n stack not valid!\n");
 }
--- source/sorts.c
+++ source/sorts.c
@@ -9,3 +9,30 @@ void	sort_two(t_container * stack)
 	}
 	print_stack(stack->stack_a);
 }
+
+void	sort_three(t_container * stack)
+{
+	int i;
+
+	i = 0;
+	while (stack->stack_a[i])
+	{
+		if (stack->stack_a[0] > stack->stack_a[1])
+		{
+			stack->stack_a = swap_sa(stack);
+			ft_printf("sa\n");
+		}
+		else if (stack->stack_a[0] > stack->stack_a[2])
+		{
+			stack->stack_a = swap_rra(stack);
+			ft_printf("rra\n");
+		}
+		else if (stack->stack_a[1] > stack->stack_a[2])
+		{
+			stack->stack_a = swap_rra(stack);
+			ft_printf("rra\n");
+		}
+		i++;
+	}
+	print_stack(stack->stack_a);
+}
